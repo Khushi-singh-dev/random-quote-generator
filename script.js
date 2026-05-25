2aeb9d508e6760a487f1f8572c21c493a2a83df6
Student KhushiSingh <Student@khushisingh.in>
2026-05-25 19:40:08 +0530

    add navbar and responsiveness

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 let colors = [
   "linear-gradient(to right, #ff5733, #ff7151, #ffb593)", 
-  "linear-gradient(to right, #15ff00, #62ff54, #a4ff90)", 
+  "linear-gradient(to right, #0a7800, #3b7d35, #5b8652)", 
   "linear-gradient(to right, #0059ff, #3795ff, #9cd9ff)", 
   "linear-gradient(to right, #ff0090, #ff4bb1, #ff8fcd)", 
   "linear-gradient(to right, #9000ff, #b14aff, #e881ff)", 
-  "linear-gradient(to right, #00fff2, #6ffff8, #b6fbff)", 
   "linear-gradient(to right, #ff0000, #ff4b4b, #ff9e9e)", 
-  "linear-gradient(to right, #ffd500, #ffe358, #ffec84)"];
+  "linear-gradient(to right, #f3b600, #caa73d, #ffe284)"];
 
 async function getQuote() {
   try{
